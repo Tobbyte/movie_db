2659eb7f9f478691559ea88860d81b7c2845204c
pun narrans <pun.narrans@gmail.com>
2026-06-23 16:33:30 +0200

    validate input in menu

--- movies.py
+++ movies.py
@@ -260,16 +260,13 @@ def present_menu(menu_items: list[str]):
 
     for item in menu_items:
         output(item)
+    selection = None
+    while selection is None:
+        selection = input("\nEnter choice (1-9): ")
 
-    selection = input("\nEnter choice (1-9): ")
-
-    if len(selection) > 1 or not selection.isdecimal():
-        output(
-            "Invalid input (Enter 0 - 9. Try again)",
-            space_before=True,
-            space_after=True,
-        )
-        return False
+        if len(selection) > 1 or not selection.isdecimal() or selection == "":
+            output("Invalid input (Enter 0 - 9. Try again)")
+            selection = None
 
     return int(selection)
 
