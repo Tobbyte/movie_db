4283a96f668e780a981da2b1a3fa036a92c08b98
pun narrans <pun.narrans@gmail.com>
2026-06-23 14:56:50 +0200

    formatting: 4 spaces (thanks msit..)

--- movies.py
+++ movies.py
@@ -1,4 +1,5 @@
 from random import randint
+
 """ A simple interface to interact with an dummy movie "db" (local dict) """
 
 """
@@ -20,199 +21,203 @@ TODO (but out of scope of this exercise):
 Version 1.0.0
 """
 
+
 def main():
-  # Dictionary to store the movies and the rating
-  movies = {
-      "The Shawshank Redemption": 9.5,
-      "Pulp Fiction": 8.8,
-      "The Room": 3.6,
-      "The Godfather": 9.2,
-      "The Godfather: Part II": 9.0,
-      "The Dark Knight": 9.0,
-      "12 Angry Men": 8.9,
-      "Everything Everywhere All At Once": 8.9,
-      "Forrest Gump": 8.8,
-      "Star Wars: Episode V": 8.7
-  }
+    # Dictionary to store the movies and the rating
+    movies = {
+        "The Shawshank Redemption": 9.5,
+        "Pulp Fiction": 8.8,
+        "The Room": 3.6,
+        "The Godfather": 9.2,
+        "The Godfather: Part II": 9.0,
+        "The Dark Knight": 9.0,
+        "12 Angry Men": 8.9,
+        "Everything Everywhere All At Once": 8.9,
+        "Forrest Gump": 8.8,
+        "Star Wars: Episode V": 8.7,
+    }
 
-  run(movies)
-
-def sort_by_value(dic: dict[str, float], reverse = False ):
-  """ Sorts a dict by its values """
-
-  return sorted(dic.items(),key=lambda item: item[1], reverse=reverse)
+    run(movies)
 
 
-def list_movies(db: dict[str, float],message,  descending = False, by_value = False):
-  """ Returns a list of all db items """
+def sort_by_value(dic: dict[str, float], reverse=False):
+    """Sorts a dict by its values"""
 
-  output(message, space_before=True)
-  if not by_value:
-    for k,v in db.items():
-      output(f"{k}: {v}")
-  else:
-    for k,v in sort_by_value(db, reverse= descending):
-      output(f"{k}: {v}")
+    return sorted(dic.items(), key=lambda item: item[1], reverse=reverse)
+
+
+def list_movies(db: dict[str, float], message, descending=False, by_value=False):
+    """Returns a list of all db items"""
+
+    output(message, space_before=True)
+    if not by_value:
+        for k, v in db.items():
+            output(f"{k}: {v}")
+    else:
+        for k, v in sort_by_value(db, reverse=descending):
+            output(f"{k}: {v}")
 
 
 def is_num(inp: str):
-  """ Validates if a sting input is a valid number"""
+    """Validates if a sting input is a valid number"""
 
-  if inp == "":
-      return False
-  try:
-    float(inp)
-  except ValueError:
-    return False
-  return True
+    if inp == "":
+        return False
+    try:
+        float(inp)
+    except ValueError:
+        return False
+    return True
 
 
 def add_movie(db):
-  """ Adds an item to db. """
+    """Adds an item to db."""
 
-  name = None
-  rating = None
+    name = None
+    rating = None
 
-  while name is None or name == "":
-    name = input("\nEnter new movie name: ")
-    if name == "":
-      output("Name required")
+    while name is None or name == "":
+        name = input("\nEnter new movie name: ")
+        if name == "":
+            output("Name required")
 
-  while rating is None or rating == "":
-    rating = input("Enter new movies rating (0-10): ")
-    if rating == "":
-      output("Rating required")
-    elif not is_num(rating):
-      rating = None
-      output("Rating must be a number")
-    elif float(rating) > 10:
-      rating = None
-      output("Rating must be between 0 - 10")
+    while rating is None or rating == "":
+        rating = input("Enter new movies rating (0-10): ")
+        if rating == "":
+            output("Rating required")
+        elif not is_num(rating):
+            rating = None
+            output("Rating must be a number")
+        elif float(rating) > 10:
+            rating = None
+            output("Rating must be between 0 - 10")
 
-  
-  db[name] = float(rating)
+    db[name] = float(rating)
 
-  output(f'Movie "{name}" with rating {rating} successfully added', space_before=True)
-  return db
+    output(f'Movie "{name}" with rating {rating} successfully added', space_before=True)
+    return db
 
 
 def del_movie(db: dict[str, float]):
-  """ Removes an item from db """
+    """Removes an item from db"""
 
-  tbdeleted = None
+    tbdeleted = None
 
-  while tbdeleted is None or tbdeleted == "":
-    tbdeleted = input("\nEnter movie name to delete: ")
-    if tbdeleted == "":
-      output("Name required")
-    try: 
-      del db[tbdeleted]
-      output(f'Movie "{tbdeleted}" successfully deleted', space_before=True)
+    while tbdeleted is None or tbdeleted == "":
+        tbdeleted = input("\nEnter movie name to delete: ")
+        if tbdeleted == "":
+            output("Name required")
+        try:
+            del db[tbdeleted]
+            output(f'Movie "{tbdeleted}" successfully deleted', space_before=True)
 
-      return db
+            return db
 
-    except KeyError:
-      output(f"Movie {tbdeleted} doesn't exist!", space_before=True)
-      break
-  return db
+        except KeyError:
+            output(f"Movie {tbdeleted} doesn't exist!", space_before=True)
+            break
+    return db
 
 
 def update_movie(db: dict[str, float]):
-  """ Updates db item. """
-  tbupdated = None
-  new_rating = None
+    """Updates db item."""
+    tbupdated = None
+    new_rating = None
 
-  while tbupdated is None or tbupdated == "":
-    tbupdated = input("\nEnter movie name to update: ")
-    if tbupdated == "":
-      output("Name required")
-    try: 
-      db[tbupdated]
-    except KeyError:
-      output(f"Movie {tbupdated} doesn't exist!", space_before=True)
-      return db
+    while tbupdated is None or tbupdated == "":
+        tbupdated = input("\nEnter movie name to update: ")
+        if tbupdated == "":
+            output("Name required")
+        try:
+            db[tbupdated]
+        except KeyError:
+            output(f"Movie {tbupdated} doesn't exist!", space_before=True)
+            return db
 
-  while new_rating is None or new_rating == "":
-    new_rating = input("Enter new movies rating (0-10): ")
-    if new_rating == "":
-      output("Rating required")
-    elif not is_num(new_rating):
-      new_rating = None
-      output("Rating must be a number")
-    elif float(new_rating) > 10:
-      new_rating = None
-      output("Rating must be between 0 - 10")
-  
-  output(f'Movie "{tbupdated}" successfully updated to rating: {new_rating}', space_before=True)
-  return db
+    while new_rating is None or new_rating == "":
+        new_rating = input("Enter new movies rating (0-10): ")
+        if new_rating == "":
+            output("Rating required")
+        elif not is_num(new_rating):
+            new_rating = None
+            output("Rating must be a number")
+        elif float(new_rating) > 10:
+            new_rating = None
+            output("Rating must be between 0 - 10")
+
+    output(
+        f'Movie "{tbupdated}" successfully updated to rating: {new_rating}',
+        space_before=True,
+    )
+    return db
 
 
 def get_average(nums: list[float]):
-  """ returns average """
-  return sum(nums) / len(nums)
+    """returns average"""
+    return sum(nums) / len(nums)
 
 
 def get_median(nums: list[float]):
-  """ returns median """
+    """returns median"""
 
-  sorted_nums = sorted(nums)
-  if len(sorted_nums) %2 != 0:
-    return sorted_nums[len(sorted_nums)//2]
-  else:
-    centeri = len(sorted_nums) // 2
-    return get_average(sorted_nums[centeri-1:centeri+1])
+    sorted_nums = sorted(nums)
+    if len(sorted_nums) % 2 != 0:
+        return sorted_nums[len(sorted_nums) // 2]
+    else:
+        centeri = len(sorted_nums) // 2
+        return get_average(sorted_nums[centeri - 1 : centeri + 1])
 
 
 def get_statistics(db: dict[str, float]):
-  """
-  Gets statistic:
-  - average
-  - median
-  - top-ranked items
-  - bottom-ranged items
-  """
+    """
+    Gets statistic:
+    - average
+    - median
+    - top-ranked items
+    - bottom-ranged items
+    """
 
-  val_list = list(db.values())
-  avg = get_average(val_list)
-  median = get_median(sorted(val_list))
-  best_name, best_rat = sort_by_value(db)[-1]
-  worst_name, worst_rat = sort_by_value(db)[0]
+    val_list = list(db.values())
+    avg = get_average(val_list)
+    median = get_median(sorted(val_list))
+    best_name, best_rat = sort_by_value(db)[-1]
+    worst_name, worst_rat = sort_by_value(db)[0]
 
-  output(f"Average rating: {avg}", space_before=True)
-  output(f"Median rating: {median}")
-  output(f'Best movie: "{best_name}", {best_rat}')
-  output(f'Worst movie: "{worst_name}", {worst_rat}')
+    output(f"Average rating: {avg}", space_before=True)
+    output(f"Median rating: {median}")
+    output(f'Best movie: "{best_name}", {best_rat}')
+    output(f'Worst movie: "{worst_name}", {worst_rat}')
 
 
 def get_random(db):
-  """ Returns random movie """
+    """Returns random movie"""
 
-  name, rating = list(db.items())[randint(0, len(db)-1)]
-  output(f"Your movie for tonight: {name}, it's rated {rating}", space_before=True)
+    name, rating = list(db.items())[randint(0, len(db) - 1)]
+    output(f"Your movie for tonight: {name}, it's rated {rating}", space_before=True)
 
 
 def search_movie(db: dict[str, float]):
-  """ Searches for items. Not case sensitive """
+    """Searches for items. Not case sensitive"""
 
-  inp = input("\nEnter part of movie name: ").lower()
-  res = [(k, v) for k, v in db.items() if k.lower().find(inp) != -1]
+    inp = input("\nEnter part of movie name: ").lower()
+    res = [(k, v) for k, v in db.items() if k.lower().find(inp) != -1]
 
-  if not res:
-    output(f'No Movie name contains "{inp}":\n')
-  else :
-    output(f'Movie titles containing "{inp}":\n', space_before=True)
-    for r in res:
-      output(f"{r[0]}, {r[1]}")
+    if not res:
+        output(f'No Movie name contains "{inp}":\n')
+    else:
+        output(f'Movie titles containing "{inp}":\n', space_before=True)
+        for r in res:
+            output(f"{r[0]}, {r[1]}")
 
-  
+
 def idle_after_input():
-  """ Idles with prompt to continue """
-  input("\npress Enter to continue")
+    """Idles with prompt to continue"""
+    input("\npress Enter to continue")
 
 
 def present_menu(menu_items: list[str]):
-  """ Prints the menu to the user, asks for input. """
-  """ Options: 
+    """Prints the menu to the user, asks for input."""
+    """ Options: 
   1. List movies, no input. Print. Return to menu.
   2. Add movie, single input:
     - str, int:[1-10] (not validated). Print new Entry. Return to menu.
@@ -229,90 +234,94 @@ def present_menu(menu_items: list[str]):
   9. Exit.
   """
 
-  output("", space_before=True)
+    output("", space_before=True)
 
-  for item in menu_items:
-    output(item)
+    for item in menu_items:
+        output(item)
 
-  selection = input("\nEnter choice (1-9): ")
+    selection = input("\nEnter choice (1-9): ")
 
-  if len(selection) > 1 or not selection.isdecimal():
-    output("Invalid input (Enter 0 - 9. Try again)", space_before=True, space_after=True)
-    return False
-  
-  return int(selection)
+    if len(selection) > 1 or not selection.isdecimal():
+        output(
+            "Invalid input (Enter 0 - 9. Try again)",
+            space_before=True,
+            space_after=True,
+        )
+        return False
+
+    return int(selection)
 
 
 def clear_screen():
-  """ Clear console hack """
-  print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    """Clear console hack"""
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
 
-def output(any, space_after = False, space_before = False):
-  """ Prints what's given. Optionally adds gap """
+def output(any, space_after=False, space_before=False):
+    """Prints what's given. Optionally adds gap"""
 
-  if space_before: print("\n \n")
-  print(any)
-  if space_after: print("\n \n")
+    if space_before:
+        print("\n \n")
+    print(any)
+    if space_after:
+        print("\n \n")
 
 
 def run(db: dict[str, float]):
-  """ Prints welcome and loops menu """
+    """Prints welcome and loops menu"""
 
-  output("********** My Movies Database **********",space_before=True)
+    output("********** My Movies Database **********", space_before=True)
 
-  menu_items= [
-    "Menu:",
-    "1. List movies",
-    "2. Add movie",
-    "3. Delete movie",
-    "4. Update movie",
-    "5. Stats",
-    "6. Random movie",
-    "7. Search movie",
-    "8. Movies sorted by rating",
-    "9. Quit",
-  ]
+    menu_items = [
+        "Menu:",
+        "1. List movies",
+        "2. Add movie",
+        "3. Delete movie",
+        "4. Update movie",
+        "5. Stats",
+        "6. Random movie",
+        "7. Search movie",
+        "8. Movies sorted by rating",
+        "9. Quit",
+    ]
 
-  while True:
-    clear_screen()
-    selection = present_menu(menu_items)
-    clear_screen()
-    output(f"~~~~~~~~~~\nSelected menu item: {menu_items[selection]}\n~~~~~~~~~~")
+    while True:
+        clear_screen()
+        selection = present_menu(menu_items)
+        clear_screen()
+        output(f"~~~~~~~~~~\nSelected menu item: {menu_items[selection]}\n~~~~~~~~~~")
 
-    if selection == 1:
-      """ list """
-      list_movies(db, f"{len(db)} movies in total:\n")
-    elif selection == 2:
-      """ add """
-      db = add_movie(db)
-    elif selection == 3:
-      """ delete """
-      db = del_movie(db)
-    elif selection == 4:
-      """ update """
-      db = update_movie(db)
-    elif selection == 5:
-      """ stats """
-      get_statistics(db)
-    elif selection == 6:
-      """ random """
-      get_random(db)
-    elif selection == 7:
-      """ search """
-      search_movie(db)
-    elif selection == 8:
-      """ list by rating """
-      list_movies(db,"Movies by rating:\n", descending=True, by_value=True)
+        if selection == 1:
+            """ list """
+            list_movies(db, f"{len(db)} movies in total:\n")
+        elif selection == 2:
+            """ add """
+            db = add_movie(db)
+        elif selection == 3:
+            """ delete """
+            db = del_movie(db)
+        elif selection == 4:
+            """ update """
+            db = update_movie(db)
+        elif selection == 5:
+            """ stats """
+            get_statistics(db)
+        elif selection == 6:
+            """ random """
+            get_random(db)
+        elif selection == 7:
+            """ search """
+            search_movie(db)
+        elif selection == 8:
+            """ list by rating """
+            list_movies(db, "Movies by rating:\n", descending=True, by_value=True)
 
-    elif selection == 9:
-      output("Goodbye")
-      return False
+        elif selection == 9:
+            output("Goodbye")
+            return False
 
-    idle_after_input()
-    
-
+        idle_after_input()
 
 
 if __name__ == "__main__":
-  main()
+    main()
