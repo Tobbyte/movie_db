ac4224b9d72120e6b1d6fc8a8f26ad39179889be
pun narrans <pun.narrans@gmail.com>
2026-06-29 09:17:17 +0200

    strip histogram input

--- movies.py
+++ movies.py
@@ -277,7 +277,7 @@ def ratings_histogram(db: list[float]):
     while filename is None or filename == "":
         filename = user_input(
             "Enter filename (saved as png unless otherwise specified in your current working directory): "
-        )
+        ).strip()
         if filename == "":
             output("Filename required", color="red")
         elif not filename.replace(".", "").isalnum():
