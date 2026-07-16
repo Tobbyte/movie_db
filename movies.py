82194b0ff08697e4c3295359e7d2a0e5af15c50a
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-07-16 15:34:40 +0200

    unneccessary var

--- movies.py
+++ movies.py
@@ -174,7 +174,6 @@ def _strip_leading_zero(num: str | float) -> str | int | float:
 
 def _get_movie_name(prompt: str) -> str:
     """Ask user to input a movies name."""
-    name = None
     while True:
         name = _get_user_input_colored(prompt).strip()
         if name != "":
